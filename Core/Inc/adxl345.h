f22ff9a3fa7370036883d70e739a892a1e010c86
Mehmet Demirbas <mehmet1demirbas@gmail.com>
2025-11-30 13:53:29 +0300

    gitignore eklendi ve cache temizlendi

---
+++ Core/Inc/adxl345.h
@@ -0,0 +1,17 @@
+/*
+ * adxl345.h
+ *
+ *  Created on: Nov 30, 2025
+ *      Author: Mehmet Demirbaş
+ */
+
+#ifndef INC_ADXL345_H_
+#define INC_ADXL345_H_
+
+#include "main.h"
+
+
+
+
+#endif /* INC_ADXL345_H_ */
+
---
+++ Core/Src/adxl345.c
@@ -0,0 +1,9 @@
+/*
+ * adxl345.c
+ *
+ *  Created on: Nov 30, 2025
+ *      Author: Mehmet Demirbaş
+ */
+
+#include "adxl345.h"
+
